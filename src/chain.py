87306393d516fe149a2606e682df103ef3e70bbe
Elizabeth Hanov <152470785+breezylizzy@users.noreply.github.com>
2025-12-10 19:20:49 +0700

    Refactor RAG chain function and add caching

--- src/chain.py
+++ src/chain.py
@@ -1,4 +1,4 @@
-# src/rag_chain.py
+import streamlit as st
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.runnables.history import RunnableWithMessageHistory
@@ -8,12 +8,13 @@ from src.memory import get_session_history
 from src.reranker import get_rerank_retriever
 
 
-def create_rag_chain(chat_model):
+@st.cache_resource
+def load_rag_chain(chat_model):
     qa_chain = create_stuff_documents_chain(
         llm=chat_model,
         prompt=prompt
     )
-    
+
     retriever = get_rerank_retriever()
 
     rag_chain = create_retrieval_chain(

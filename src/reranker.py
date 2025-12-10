cad8ed7a3667a95d1d523ec7b8e7da230bbb045e
Elizabeth Hanov <152470785+breezylizzy@users.noreply.github.com>
2025-12-10 19:20:16 +0700

    Refactor reranker and base retriever loading
    Refactor reranker initialization to use Streamlit caching and add a load_base_retriever function.

--- src/reranker.py
+++ src/reranker.py
@@ -1,15 +1,22 @@
+import torch
 from sentence_transformers import CrossEncoder
 from langchain_core.runnables import RunnableLambda
 from langchain.schema import Document
 from typing import List
-import torch
+import streamlit as st
 
 from src.retriever import get_base_retriever
 
-reranker = CrossEncoder(
-    "BAAI/bge-reranker-base",
-    device="cuda" if torch.cuda.is_available() else "cpu"
-)
+
+@st.cache_resource
+def load_reranker():
+    return CrossEncoder(
+        "BAAI/bge-reranker-base",
+        device="cuda" if torch.cuda.is_available() else "cpu"
+    )
+
+reranker = load_reranker()
+
 
 def rerank_docs(query: str, docs: List[Document], top_k: int = 5):
     pairs = [[query, d.page_content] for d in docs]
@@ -18,12 +25,22 @@ def rerank_docs(query: str, docs: List[Document], top_k: int = 5):
     ranked = sorted(zip(scores, docs), key=lambda x: x[0], reverse=True)
     return [doc for score, doc in ranked[:top_k]]
 
+
+@st.cache_resource
+def load_base_retriever(k=20):
+    return get_base_retriever(k=k)
+
+
 def get_rerank_retriever(top_k_retriever=20, top_k_rerank=5):
-    base_retriever = get_base_retriever(k=top_k_retriever)
+
+    base_retriever = load_base_retriever(k=top_k_retriever)
 
     def rerank_retriever_fn(input_data):
-        query = input_data["input"] if isinstance(input_data, dict) else str(input_data)
-
+        query = (
+            input_data["input"]
+            if isinstance(input_data, dict)
+            else str(input_data)
+        )
         docs = base_retriever.invoke(query)
         return rerank_docs(query, docs, top_k=top_k_rerank)
 
